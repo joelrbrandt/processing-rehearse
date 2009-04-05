4842850b7d0e03b42cfc6fe759f82e241bec6a05
ben.hsieh <ben.hsieh@7caee240-1f58-11de-a7a1-25a91d9625e5>
2009-04-05 02:19:56 +0000

    size() and background()

--- src/edu/stanford/hci/processing/ProcessingMethods.java
+++ src/edu/stanford/hci/processing/ProcessingMethods.java
@@ -3,15 +3,21 @@ package edu.stanford.hci.processing;
 import java.awt.Canvas;
 import java.awt.Color;
 
+import javax.swing.JFrame;
+
 public class ProcessingMethods {
 	Canvas canvas;
-	public ProcessingMethods (Canvas c) {
+	JFrame frame;
+	
+	public ProcessingMethods (Canvas c, JFrame frame) {
 		this.canvas = c;
+		this.frame = frame;
 	}
 	
 	public void size(int width, int height) {
 		System.out.println("HI GUYS SIZE GOT CALLED");
 		canvas.setSize(width, height);
+		frame.pack();
 	}
 	
 	public void background(int r, int g, int b) {
