74f5dc7037462aa128420deffd781f5f7016233e
joelrbrandt <joelrbrandt@7caee240-1f58-11de-a7a1-25a91d9625e5>
2009-05-25 00:40:12 +0000

    found problems, added them to broken list

--- src/edu/stanford/hci/BeanShellTester.java
+++ src/edu/stanford/hci/BeanShellTester.java
@@ -1,5 +1,7 @@
 package edu.stanford.hci;
 
+import java.awt.Point;
+
 import bsh.*;
 
 public class BeanShellTester {
@@ -8,6 +10,10 @@ public class BeanShellTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Point p = new Point(2,3);
+		p.x = 3;
+		p.x += 1.0f;
+		System.out.println("p.x: " + p.x);
 		Interpreter i = new Interpreter();
 		try {
 			i.eval("class Foo { static Bar b; }");
